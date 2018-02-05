0402bb4ccacfa8966029430f1a135dabe81389fd
Masiukevich Maksim <maksim.masiukevich@distate.dsnet>
2018-02-05 17:31:16 +0300

    service bus update

--- src/Application/ApplicationContext.php
+++ src/Application/ApplicationContext.php
@@ -13,8 +13,10 @@ declare(strict_types = 1);
 namespace Desperado\ServiceBusDemo\Application;
 
 use Desperado\Domain\Transport\Context\OutboundMessageContextInterface;
+use Desperado\Infrastructure\Bridge\Logger\LoggerRegistry;
 use Desperado\ServiceBus\MessageProcessor\AbstractExecutionContext;
 use Desperado\ServiceBus\Transport\Context\OutboundMessageContext;
+use Psr\Log\LoggerInterface;
 
 /**
  * Application-level context
@@ -31,6 +33,14 @@ class ApplicationContext extends AbstractExecutionContext
     /**
      * @inheritdoc
      */
+    public function getLogger(string $channelName): LoggerInterface
+    {
+        return LoggerRegistry::getLogger($channelName);
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function applyOutboundMessageContext(OutboundMessageContextInterface $outboundMessageContext): self
     {
         $self = new self();
@@ -43,7 +53,7 @@ class ApplicationContext extends AbstractExecutionContext
     /**
      * @inheritdoc
      */
-    public function getOutboundMessageContext(): ?OutboundMessageContext
+    public function getOutboundMessageContext(): ?OutboundMessageContextInterface
     {
         return $this->outboundMessageContext;
     }
